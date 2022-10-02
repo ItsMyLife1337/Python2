298b16a7d549089c037c53918b0d52d2253f50c6
ItsMyLife1337 <itsmylife1337@yandex.ru>
2022-10-02 21:57:24 +0300

    Добавление задания №2 + исправление №1

--- Zadanie 1.py
+++ Zadanie 1.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-if __name__ == "__main__":
-    a = set("a, o, u, e, i, y, а, и, е, ё, у, о, ы, э, ю, я")
-    n = int(input('Количество элементов множества?'))
-    m = set()
-    for i in range(n):
-        m.add(input("введите элемент - "))
-    b = a.intersection(m)
-    print(m)
-    print("Количество гласных ", len(b))
+# _*_ coding: utf-8 _*_
+
+if __name__ == '__main__':
+    strock = input("Введите строку: ").lower()
+    a = set(strock)
+    u = set("aeiouyаоуыэеёиюя")
+
+    count = 0
+    for i in strock:
+        if i in u:
+            count += 1
+
+    print(count)
